fcbaefc07c08d092c0421df2051d21ea3db5d6f7
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-01 16:56:00 +0530

    Add files via upload

---
+++ Day3Assignment2.java
@@ -0,0 +1,61 @@
+package Rohit;
+
+import java.util.Scanner;
+
+class Shape{
+	void draw() {
+		System.out.println("Drawing Shape");
+		
+	}
+	void erase() {
+		System.out.println("Erasing Shape");
+		
+	}
+	
+}
+class Circle extends Shape{
+	@Override
+	void draw() {
+		System.out.println("Drawing Cicle");
+	}
+	void erase() {
+		System.out.println("Erasing Circle");
+	}
+}
+
+class Triangle extends Shape{
+	@Override
+	void draw() {
+		System.out.println("Drawing Triangle");
+	}
+	void erase() {
+		System.out.println("Erasing Triangle");
+	}
+}
+
+class Square extends Shape{
+	@Override
+	void draw() {
+		System.out.println("Drawing Square");
+	}
+	void erase() {
+		System.out.println("Erasing Square");
+	}
+}
+
+public class Assignment2Day3 {
+
+	public static void main(String[] args) {
+		Shape c = new Circle();
+		Shape t = new Triangle();
+		Shape s = new Square();
+		c.draw();
+		c.erase();
+		t.draw();
+		t.erase();
+		s.draw();
+		s.erase();
+
+	}
+
+}
