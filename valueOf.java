e905cfd0d5fb61a136e93a5a3ec0c2fb04a41c0d
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 15:06:33 +0530

    Add files via upload

---
+++ valueOf.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class valueOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 50;
+		String s = String.valueOf(n);
+		
+		System.out.println(s+30);
+
+	}
+
+}
