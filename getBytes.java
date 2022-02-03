4d01cb5e059f6c29854cb70343a117f18fd8eed5
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:46:28 +0530

    Add files via upload

---
+++ getBytes.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class getBytes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Rohit";
+		System.out.println(s.getBytes());
+
+	}
+
+}
