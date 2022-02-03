a2e30a0b384e6e3bd6c1d4c3ce99f678bc738f42
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:48:23 +0530

    Add files via upload

---
+++ join.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class join {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sj = String.join(" ","Rohit","is","good");
+		System.out.println(sj);
+
+
+	}
+
+}
