fc08602216012798502cee6ec9fec3b13d16f7f6
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:47:08 +0530

    Add files via upload

---
+++ indexOf.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class indexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Rohit";
+		System.out.println(s.indexOf("h"));
+
+	}
+
+}
