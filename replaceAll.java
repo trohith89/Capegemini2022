ccf2959727e035cc7c212f7365028dd3601a5a77
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:55:19 +0530

    Add files via upload

---
+++ replaceAll.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class replaceAll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Rohit is a good boy";
+		String s2 = "rohit";
+		System.out.println(s1.replaceAll("is","was"));
+
+
+	}
+
+}
