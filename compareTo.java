55ecfe1b498826d7e6123c91bdca36ac31a28293
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:42:54 +0530

    Add files via upload

---
+++ compareTo.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class compareTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Rohit";
+		String s2 = "rohit";
+		System.out.println(s1.compareTo(s2));
+
+	}
+
+}
