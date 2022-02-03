121d9bea4be37e19578e77b9261e6342334ddc13
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:45:44 +0530

    Add files via upload

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class equalsIgnoreCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Rohit";
+		String s2 = "rohit";
+		System.out.println(s1.equalsIgnoreCase(s2));
+
+	}
+
+}
