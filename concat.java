a7f96f86057301ff4fa1743447d77e4e2ed0a596
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:43:14 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class concat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Rohit";
+		String s2 = "rohit";
+		System.out.println(s1.concat(s2));
+
+	}
+
+}
