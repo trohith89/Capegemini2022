66238ef03ca004c91c6dd4e674cefb81446ce937
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:46:02 +0530

    Add files via upload

---
+++ format.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class format {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Rohit";
+		
+		String sf1 = String.format("name is %s",s);
+		System.out.println(sf1);
+
+	}
+
+}
