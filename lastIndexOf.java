25320aad5722d0394d27702de57a3b528b7a5562
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:54:00 +0530

    Add files via upload

---
+++ lastIndexOf.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class lastIndexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Rohit";
+		String s2 = "rohit";
+		System.out.println(s1.lastIndexOf(2));
+
+
+	}
+
+}
