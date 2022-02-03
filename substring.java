d40076a3c229d383385dd9e24ab57342b36855e0
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 14:57:28 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Rohit";
+		String s2 = "ohit";
+		System.out.println(s1.substring(0,2));
+
+
+	}
+
+}
