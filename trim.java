ab1c7078b42c8add162e3121d30ec97de1ef6807
trohith89 <75958140+trohith89@users.noreply.github.com>
2022-02-03 15:06:05 +0530

    Add files via upload

---
+++ trim.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class trim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = " Rohit is a good boy ";
+		String s2 = "rohit";
+		System.out.println(s1.trim());
+
+
+	}
+
+}
